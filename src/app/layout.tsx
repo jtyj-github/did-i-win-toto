3715323979f81f1ce2e6b1bce1db4850af0051d5
Andre H <andrehadiantolesmana@gmail.com>
2024-06-08 08:26:42 +0800

    ♻️ refactor: add and use Inter font file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
 import React from 'react';
 
-const inter = Inter({ subsets: ['latin'] });
+import { inter } from '@/common/components/font/Inter';
+
+import './globals.css';
 
 export const metadata: Metadata = {
     title: 'Create Next App',
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config: Config = {
     content: [
@@ -8,6 +9,9 @@ const config: Config = {
     ],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ['var(--font-inter)', ...fontFamily.sans]
+            },
             colors: {
                 link: '#2563EB',
                 primary: {
