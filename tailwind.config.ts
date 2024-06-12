0b012c60772add8408f72b5fe16337457600801d
Andre H <andrehadiantolesmana@gmail.com>
2024-06-12 09:30:46 +0800

    Squashed commit of the following:
    commit e0e92d78deb9f74ea59b4ffced0f6fb743e96c1c
    Merge: 7eb3109 f1951f7
    Author: Justin Tan <131244038+jtyj-github@users.noreply.github.com>
    Date:   Sun Jun 9 00:42:18 2024 +0800
    
        Merge pull request #2 from jtyj-github/andrel/basic-layout
    
        feat: implement basic layout and configs
    
    commit f1951f72ef29a1d7e21607c4d21f9e1b7b99e8d6
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 08:42:12 2024 +0800
    
        ♻️ refactor: replace app.tsx with new empty layout
    
    commit d0667ddc5796f0e9505f1d23881a079dd1381c99
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 08:41:52 2024 +0800
    
        ⚡️ feat: add reusable Main layout component
    
    commit f2c7f9225357bb004e5af0171b79d4e8a5d338bf
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 08:41:39 2024 +0800
    
        ⚡️ feat: add cloneElement utils
    
    commit 34a780e31db1281c07c5e34eee24f7aeadf9fc54
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 08:41:26 2024 +0800
    
        ⚡️ feat: add Heading component
    
    commit 6281ef994c8ff88e6abf4bc5737c2f8e569509ba
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 08:36:41 2024 +0800
    
        ⚡️ feat: add cn utility
    
    commit e45801622f3cdd055c906b86a5ff6e46ece001d0
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 08:33:45 2024 +0800
    
        🎨 style: default to light mode
    
    commit 4b27ba008d242c04e3160aa1cfddd901abe28eb7
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 08:31:38 2024 +0800
    
        ⚡️ feat: add prettier: tailwind class sort
    
    commit 9048609b0230ebb362f6c8f610c9ccac005311fe
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 08:26:57 2024 +0800
    
        🔧 chore: tsconfig to include `.theme.ts` for tailwind-variants
    
    commit 3715323979f81f1ce2e6b1bce1db4850af0051d5
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 08:26:42 2024 +0800
    
        ♻️ refactor: add and use Inter font file
    
    commit 251f125057a8301238645613aeffaa9c6d4154f6
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 08:07:39 2024 +0800
    
        🎨 style: add basic color scheme
    
    commit dded2a430d588bd981272a294f9bdec225e89cc7
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 07:58:44 2024 +0800
    
        🔧 chore: warn unused var instead
    
    commit 94e3da6555e394adbcff62d04d2241c73bb60098
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 07:42:43 2024 +0800
    
        ⚡️ feat: add typescript eslint parser and plugin
    
    commit 28f75d1f2dbc229a948dcdc4cab5fc2851f002ca
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 07:38:55 2024 +0800
    
        ⚡️ feat: add new eslint rules
    
    commit 4b5377a538a22d2d94853b15aa297d8257d1acad
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 07:36:58 2024 +0800
    
        ⚡️ feat: add new eslint plugins
    
    commit 819e276be101ffea3d79e6f7ed50548938b228b7
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Sat Jun 8 07:31:04 2024 +0800
    
        🐛 fix: run lint and prettier
    
    commit 7eb3109d111cd3ede62a6a0ea107e912e89a796f
    Merge: d228f04 e61ebfe
    Author: Andre Hadianto <40631483+andrehadianto@users.noreply.github.com>
    Date:   Sat Jun 8 07:20:16 2024 +0800
    
        Merge pull request #1 from jtyj-github/backend
    
        Justin - Set up folder structure and prisma + mongodb
    
    commit e61ebfe747924236f6581a8cd69857eb01c6e634
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Thu Jun 6 20:37:38 2024 +0800
    
        🐛 fix: remove `.env` from repo
    
    commit ae66683bc2e677f4ecca346054345516e12631e2
    Author: Andre H <andrehadiantolesmana@gmail.com>
    Date:   Thu Jun 6 20:36:44 2024 +0800
    
        🐛 fix: add other `.env` to gitignore

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,62 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['var(--font-inter)', ...fontFamily.sans]
+            },
+            colors: {
+                link: '#2563EB',
+                primary: {
+                    default: '#1eeb40'
+                },
+                bg: {
+                    base: '#FAFAFA'
+                },
+                border: {
+                    base: '#DFDFE2'
+                },
+                surface: {
+                    base: '#EFEFF0',
+                    elevated: '#E7E7E9'
+                },
+                text: {
+                    'em-high': '#070708',
+                    'em-mid': '#505162',
+                    'em-low': '#73748C',
+                    'em-placeholder': '#ABABBA',
+                    'on-primary': '#080B5E',
+                    'on-secondary': '#F7F7F8',
+                    'on-tertiary': '#3C3C44',
+                    'on-tertiary-high': '#070708',
+                    'on-error': '#EF4444',
+                    error: '#DC2626',
+                    success: '#22C55E',
+                    warning: '#F59E0B'
+                },
+                element: {
+                    primary: '#1eeb40',
+                    secondary: '#EB1EC9',
+                    tertiary: '#DFDFE2',
+                    disabled: '#DDDEE4',
+                    success: '#052E16',
+                    warning: '#451A03',
+                    error: '#450A0A'
+                }
+            },
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 };
 export default config;
