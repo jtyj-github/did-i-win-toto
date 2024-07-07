3f464f4e1aa03bdef937499a378fd528d51d56bc
Justin <tanyjjustin@gmail.com>
2024-07-08 00:07:09 +0800

    :heavy_plus_sign: add: dotenv package

---
+++ src/CallScrape.ts
@@ -0,0 +1,5 @@
+import puppeteer, {Browser} from "puppeteer";
+import {TotoResult} from "@prisma/client";
+import { getJSON } from "./common/utils/processFetchedData";
+import { writeStores } from "./common/utils/readWriteStores";
+
