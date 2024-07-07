36df4aed178f4754471091e51dd2f9408e822ff3
Justin <tanyjjustin@gmail.com>
2024-07-08 00:34:33 +0800

    ::closed_lock_with_key: edit .env file

--- src/CallScrape.ts
+++ src/CallScrape.ts
@@ -1,3 +1,4 @@
+import dotenv from "dotenv"; 
 import puppeteer, {Browser} from "puppeteer";
 import {TotoResult} from "@prisma/client";
 import { getJSON } from "./common/utils/processFetchedData";
