48915aa6b01369147e6d918de56eb75daf4d0d89
Andre H <andrehadiantolesmana@gmail.com>
2024-07-22 18:51:42 +0800

    ♻️ refactor: core layout to use hscreensafe

--- src/common/components/CoreLayout/CoreLayout.tsx
+++ src/common/components/CoreLayout/CoreLayout.tsx
@@ -1,5 +1,5 @@
-import React, { PropsWithChildren } from 'react';
+import { PropsWithChildren } from 'react';
 
 export const CoreLayout = ({ children }: PropsWithChildren) => {
-    return <div className="relative h-full min-h-screen">{children}</div>;
+    return <div className="h-screen-safe relative mx-auto bg-gray-900">{children}</div>;
 };
