01309a278961989c2ccb237d8c7e2495cc01c65e
Andre H <andrehadiantolesmana@gmail.com>
2024-07-22 18:51:26 +0800

    ⚡️ feat: add custom icon component

---
+++ src/common/components/CustomIcon/Icon.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { Icon as Iconify, IconifyIconProps } from '@iconify-icon/react';
+
+type IconProps = Omit<IconifyIconProps, 'ref'>;
+
+export const Icon = (props: IconProps) => <Iconify {...props} />;
