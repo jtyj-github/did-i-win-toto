b38f347b2866270d06d7e09c4a9991b7bfaf7d7f
Justin Tan <justin@justins-laptop.lan>
2024-07-23 00:29:59 +0800

    🧪 test: add code for testing storeUserTicket functionality

--- src/pages/api/storeUserTicket.ts
+++ src/pages/api/storeUserTicket.ts
@@ -1,12 +1,19 @@
 /**  this api stores the user ticket in the mongodb database */
 import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/common/lib/prisma';
 
 const storeUserTicket = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        console.log(req.body)
-        const { ticket } = req.body;
-        console.log(ticket);
-        res.status(200).json({ ticket });
+        console.log(req.body);
+        const userTicket = await prisma.totoTicket.create({
+            data: {
+                numbers: req.body.numbers,
+                type: req.body.type,
+                date: req.body.date,
+            }
+        })
+        console.log(userTicket);
+        res.status(200).json({message: 'POST request received'});
     } else {
         res.status(405).json({ message: 'Method not allowed' });
     }
