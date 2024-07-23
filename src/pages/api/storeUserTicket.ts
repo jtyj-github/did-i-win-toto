3157f44d5a80261974ba54251f18aa974d375f3a
Justin Tan <justin@justins-laptop.lan>
2024-07-24 00:11:13 +0800

    🔧 chore: include uuid param to be stored in db

--- src/pages/api/storeUserTicket.ts
+++ src/pages/api/storeUserTicket.ts
@@ -1,21 +1,25 @@
-// Implement uuid to identify which ticket belongs to which user
-
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/common/lib/prisma';
+import getUserId from '@/common/utils/getUserId';
 
 const storeUserTicket = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        console.log(req.body);
-        const userTicket = await prisma.totoTicket.create({
-            data: {
-                numbers: req.body.numbers,
-                type: req.body.type,
-                date: req.body.date,
-                uuid: req.body.uuid
-            }
-        })
-        console.log(userTicket);
-        res.status(200).json({message: 'Ticket Successfully Stored'});
+        try {
+            const uuid = getUserId();
+            const userTicket = await prisma.totoTicket.create({
+                data: {
+                    numbers: req.body.numbers,
+                    type: req.body.type,
+                    date: req.body.date,
+                    uuid: uuid,
+                }
+            })
+            res.status(200).json({message: 'Ticket Successfully Stored', data: {userTicket}});
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Error storing ticket' });
+        }
     } else {
         res.status(405).json({ message: 'Method not allowed' });
     }
