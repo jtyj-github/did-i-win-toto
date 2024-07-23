c968683920225d8da6c5d64a8cfbfebfe3f163fe
Justin Tan <justin@justins-laptop.lan>
2024-07-24 00:35:48 +0800

    🔧 chore: next steps for getUserId

--- src/common/utils/getUserId.ts
+++ src/common/utils/getUserId.ts
@@ -1,5 +1,7 @@
+// This will be adapted and moved to the Client-side after merging the PR, where client-side code can be accessed.
+// Rationale for doing so is because localStorage is not available in the server-side
+
 function generateUuid() {
-    // Consider replacing with uuid package in the future
     return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
         (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
     );
