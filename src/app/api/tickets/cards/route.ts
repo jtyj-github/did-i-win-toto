85b9c6712eedd5dce485121d173b17823af4c78f
Justin Tan <justin@justins-laptop.lan>
2024-08-02 00:40:45 +0800

    ⚡️ feat: Add api route for retrieving toto Cards

---
+++ src/app/api/tickets/cards/route.ts
@@ -0,0 +1,12 @@
+import { readStores } from "@/common/utils/readWriteStores"
+import { TotoCardProps } from "@/modules/toto/components/TotoCard";
+
+const fileName = 'sg_lottery.json';
+
+export const GET = async () => {
+    const output = readStores<TotoCardProps>(`/${fileName}`, 'temp');
+
+    return Response.json({
+        message: 'Successfully retrieved lottery data',
+    })
+}
