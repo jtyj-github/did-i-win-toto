0fb7b52ac41b724d0834d08c4e5d4e7835d4fbf1
Andre H <andrehadiantolesmana@gmail.com>
2024-08-03 12:28:41 +0800

    🐛 fix: linting

--- src/common/components/Field/Field.tsx
+++ src/common/components/Field/Field.tsx
@@ -1,57 +1,49 @@
-import { ComponentPropsWithoutRef, ReactNode } from "react";
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
 
-import { fieldTheme, type FieldVariants } from "./Field.theme";
+import { cn } from '@/common/utils/cn';
 
-import { cn } from "@/common/functions";
+import { fieldTheme, type FieldVariants } from './Field.theme';
 
 export interface FieldProps {
-  label?: string;
-  message?: ReactNode;
-  error?: boolean;
+    label?: string;
+    message?: ReactNode;
+    error?: boolean;
 }
 
-export interface FieldFullProps
-  extends FieldProps,
-    ComponentPropsWithoutRef<"div">,
-    FieldVariants {
-  labelProps?: ComponentPropsWithoutRef<"label">;
+export interface FieldFullProps extends FieldProps, ComponentPropsWithoutRef<'div'>, FieldVariants {
+    labelProps?: ComponentPropsWithoutRef<'label'>;
 
-  htmlFor?: string;
+    htmlFor?: string;
 }
 
 export const Field = ({
-  label,
-  children,
-  message,
-  error,
-  labelProps,
-  size,
-  className,
-  htmlFor,
-  ...props
+    label,
+    children,
+    message,
+    error,
+    labelProps,
+    size,
+    className,
+    htmlFor,
+    ...props
 }: FieldFullProps) => {
-  const {
-    wrapper,
-    label: labelStyle,
-    message: messageStyle,
-  } = fieldTheme({ className, size });
-  return (
-    <div {...props} className={cn(wrapper(), className)}>
-      {label && (
-        <label
-          htmlFor={htmlFor}
-          {...labelProps}
-          className={labelStyle({ className: labelProps?.className })}
-        >
-          {label}
-        </label>
-      )}
-      {children}
-      {message && (
-        <p className={messageStyle()} data-error={error ? "" : undefined}>
-          {message}
-        </p>
-      )}
-    </div>
-  );
+    const { wrapper, label: labelStyle, message: messageStyle } = fieldTheme({ className, size });
+    return (
+        <div {...props} className={cn(wrapper(), className)}>
+            {label && (
+                <label
+                    htmlFor={htmlFor}
+                    {...labelProps}
+                    className={labelStyle({ className: labelProps?.className })}>
+                    {label}
+                </label>
+            )}
+            {children}
+            {message && (
+                <p className={messageStyle()} data-error={error ? '' : undefined}>
+                    {message}
+                </p>
+            )}
+        </div>
+    );
 };
