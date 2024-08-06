df9bf135ef0487dac22c7b499213f0825b94e44b
Justin Tan <justin@justins-laptop.lan>
2024-08-06 20:49:03 +0800

    ♻️ refactor: include try-catch to cards api

--- src/app/api/tickets/cards/route.ts
+++ src/app/api/tickets/cards/route.ts
@@ -1,13 +1,24 @@
 import { readStores } from "@/common/utils/readWriteStores"
+
 import { TotoCardProps } from "@/modules/toto/components/TotoCard";
 
 const fileName = 'sg_lottery.json';
 
 export const GET = async () => {
-    const output = readStores<TotoCardProps>(`/${fileName}`, 'temp');
 
-    return Response.json({
-        message: 'Successfully retrieved lottery data',
-        data: output
-    })
+    try {
+        const output = readStores<TotoCardProps>(`/${fileName}`, 'temp');
+    
+        return Response.json({
+            message: 'Successfully retrieved lottery data',
+            data: output,
+            status: 200
+        })     
+    } catch (error) {
+        return Response.json({
+            message: 'Error retrieving lottery data',
+            error: error,
+            status: 500
+        })
+    }
 }
