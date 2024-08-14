0d3fe3a50217db64e5595f9cda870b7242a11568
jtyj-github <tanyjjustin@gmail.com>
2024-08-15 00:37:47 +0800

    🧪test: Included useUserCards test output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,15 @@ import { useUserCards } from '@/modules/toto/hooks/useUserCards';
 export default function Home() {
     const { TotoCards } = useTotoCards();
 
+    const test = useUserCards();
+    console.log(test);
+
     //TODO: Incorporate this into the useUserCards hook
     const USER_HAS_SUBMITTED_TICKET = true;
     
     //TODO: Change this to the actual ticket submitted by the user
     const MY_SUBMITTED_TICKET = {
-        "userCards": [
+        "userTickets": [
             {
                 "drawNumbers": [
                     14,
@@ -70,7 +73,7 @@ export default function Home() {
                     <div className="space-y-4 rounded-xl border border-element-primary bg-surface-elevated p-3">
                         {MY_SUBMITTED_TICKET && (
                             <div>
-                                {MY_SUBMITTED_TICKET.userCards.map((ticket, ticketIndex) => (
+                                {MY_SUBMITTED_TICKET.userTickets.map((ticket, ticketIndex) => (
                                     <div key={ticketIndex} className="flex flex-row justify-evenly py-3">
                                     {ticket.drawNumbers.map((num, index) => (
                                     <div key={index} className="grid h-8 w-8 place-content-center">
