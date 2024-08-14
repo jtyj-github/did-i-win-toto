8960201d363028bb6dbcd3e286b8174d057fa0c9
jtyj-github <tanyjjustin@gmail.com>
2024-08-14 22:46:36 +0800

    🔧chore: Added userId in dependency array

--- src/modules/toto/hooks/useUserCards.tsx
+++ src/modules/toto/hooks/useUserCards.tsx
@@ -1,19 +1,20 @@
 import { useState, useEffect } from "react";
 
+import { UserCardProps } from "@/modules/toto/components/TotoCard";
 import { useUser } from '@/modules/toto/hooks/useUser';
 
-import { TotoCardProps } from "../components/TotoCard";
 
 export const useUserCards = () => {
-    const [userCards, setUserCards] = useState<TotoCardProps[]>([]);
+    const [userCards, setUserCards] = useState<UserCardProps[]>([]);
     const userId = useUser();
 
     useEffect(() => {
         fetch('/api/tickets', {method: "POST", body: JSON.stringify({userId})})
             .then(response => response.json())
+            .then(response => console.log(response))
             .then(response => setUserCards(response.data))
             .catch(error => console.error('An error occured in obtaining your TOTO tickets', error));
-    })
+    }, [userId]);
 
     return { userCards };
 }
