757057b7398e394132ebf85c56d8441a925e4df4
jtyj-github <tanyjjustin@gmail.com>
2024-08-17 10:01:51 +0800

    🐛fix: usercards not fetching correctly

--- src/modules/toto/hooks/useUserCards.tsx
+++ src/modules/toto/hooks/useUserCards.tsx
@@ -1,19 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-import { UserCardProps } from "@/modules/toto/components/TotoCard";
+import { UserCardProps } from '@/modules/toto/components/TotoCard';
 import { useUser } from '@/modules/toto/hooks/useUser';
-
 
 export const useUserCards = () => {
     const [userCards, setUserCards] = useState<UserCardProps[]>([]);
     const userId = useUser();
 
     useEffect(() => {
-        fetch('/api/tickets', {method: "POST", body: JSON.stringify({userId})})
-            .then(response => response.json())
-            .then(response => setUserCards(response.data))
-            .catch(error => console.error('An error occured in obtaining your TOTO tickets', error));
+        if (userId) {
+            fetch('/api/tickets', { method: 'POST', body: JSON.stringify({ userId }) })
+                .then((response) => response.json())
+                .then((response) => setUserCards(response.data))
+                .catch((error) =>
+                    console.error('An error occured in obtaining your TOTO tickets', error)
+                );
+        }
     }, [userId]);
 
     return userCards;
-}
+};
