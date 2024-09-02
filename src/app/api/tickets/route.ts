91117b2b1b2bf1d4ad5cfbf0c2dca079627f070e
jtyj-github <tanyjjustin@gmail.com>
2024-09-03 00:41:16 +0800

    🔧chore: include to do for error catching

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -2,6 +2,8 @@ import prisma from '@/common/lib/prisma';
 
 export const POST = async (req: Request) => {
     const { userId } = await req.json();
+    
+    // TODO: Include catch for calling without userId
 
     try {
         const userTickets = await prisma.totoTicket.findMany({
