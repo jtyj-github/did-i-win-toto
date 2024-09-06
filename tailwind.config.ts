770891caebb41be1cc77d32b152e9cfa1d6e38b4
Andre H <andrehadiantolesmana@gmail.com>
2024-09-06 10:16:51 +0800

    ♻️ refactor: add new tailwind tokens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,33 @@ const config: Config = {
             fontFamily: {
                 sans: ['var(--font-inter)', ...fontFamily.sans]
             },
+            keyframes: {
+                slideDownAndFade: {
+                    from: { opacity: '0', transform: 'translateY(-2px)' },
+                    to: { opacity: '1', transform: 'translateY(0)' }
+                },
+                slideLeftAndFade: {
+                    from: { opacity: '0', transform: 'translateX(2px)' },
+                    to: { opacity: '1', transform: 'translateX(0)' }
+                },
+                slideUpAndFade: {
+                    from: { opacity: '0', transform: 'translateY(2px)' },
+                    to: { opacity: '1', transform: 'translateY(0)' }
+                },
+                slideRightAndFade: {
+                    from: { opacity: '0', transform: 'translateX(2px)' },
+                    to: { opacity: '1', transform: 'translateX(0)' }
+                }
+            },
+            animation: {
+                slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+                slideLeftAndFade: 'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+                slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+                slideRightAndFade: 'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)'
+            },
+            animationTimingFunction: {
+                'out-back': 'cubic-bezier(0.34, 1.3, 0.64, 1)'
+            },
             colors: {
                 primary: {
                     default: '#2B71E9'
